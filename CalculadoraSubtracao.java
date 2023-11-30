2d8bbe4ade36b0e161b42527ede5094146b85f50
mateus Camurça <mateuscamurcaoficial@gmail.com>
2023-11-29 22:42:22 -0300

    Contribuicao codigo

--- CalculadoraSubtracao.java
+++ CalculadoraSubtracao.java
@@ -6,4 +6,12 @@ public class CalculadoraSubtracao {
     public int subtrair(int a, int b, int c) {
         return a - b - c;
     }
+    
+    public static void main(String[] args) {
+        CalculadoraSubtracao calculadora = new CalculadoraSubtracao();
+        int resultadoSubtracao2 = calculadora.subtrair(5, 3);
+        int resultadoSubtracao3 = calculadora.subtrair(10, 4, 2);
+        System.out.println("Resultado da subtração 2: " + resultadoSubtracao2);
+        System.out.println("Resultado da subtração 3: " + resultadoSubtracao3);
+    }
 } 
